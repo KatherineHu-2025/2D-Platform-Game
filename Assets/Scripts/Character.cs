109fd20cc7b4263142fc0534a30cc31547957e2e
Katherine Hu <hymm0825@gmail.com>
2024-02-15 16:53:46 +0100

    character movement

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    private SpriteRenderer _playerSpriteRenderer;
+    private Rigidbody2D _playerRigidBody;
+    private Animator _playerAnimator;
+    public float speed;
+    private bool s_movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        _playerRigidBody = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerMovement();
+    }
+
+    void playerMovement(){
+        Vector3 velocity = new Vector3(0.0f,0.0f,0.0f);
+
+        if (Input.GetKey(KeyCode.D))
+        {   
+            velocity += Vector3.right;
+            _playerAnimator.SetTrigger("isMoving");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity += Vector3.left;
+            _playerAnimator.SetTrigger("isMoving");
+        }
+
+        _playerRigidBody.velocity = velocity * Time.deltaTime * speed;
     }
 }
