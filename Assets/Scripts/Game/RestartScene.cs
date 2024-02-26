e47d0cedb8448cdc2df5c209fdace0c17e5eea76
Linda Wang <zw101@wellesley.edu>
2024-02-26 10:15:46 -0500

    boyphase scene, nature tile map, platform + loadscene + restart scripts

---
+++ Assets/Scripts/Game/RestartScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+    private void Update()
+    {
+
+
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collision entered");
+        SceneManager.LoadScene("BoyPhase");
+    }
+}
