fb6eda031ba17573baacf9dff995dab6a0ea9d31
Linda Wang <zw101@wellesley.edu>
2024-03-01 11:28:37 -0500

    all boy scenes done + updated boyphase scene and camerafollows script

--- Assets/Scripts/Game/CameraFollows.cs
+++ Assets/Scripts/Game/CameraFollows.cs
@@ -9,6 +9,10 @@ public class CameraFollows : MonoBehaviour
 
     // Update is called once per frame
     void LateUpdate () {
+<<<<<<< HEAD
+        transform.position = player.transform.position + new Vector3(1, 0, -5);
+=======
         transform.position = player.transform.position + new Vector3(0, 0, -5);
+>>>>>>> 0e0447a56c27b2733b24e7b6ff62aa12bca21c38
     }
 }
